e43d06e23d7529f476a02b003994da3d4f6f894f
mshivateja <shivatejam123@gmail.com>
2025-01-29 11:28:25 +0530

    first commit

---
+++ Day3/Cust.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    internal class Cust
+    {
+        int a;
+        int b;
+        public Cust(int A, int B)
+        {
+            
+            a = A;
+            b = B;
+
+        }
+
+        public void CValue()
+        {
+
+            Console.WriteLine($"A value {a}");
+            Console.WriteLine($"B value {b}");
+        }
+    }
+}
---
+++ Day3/Program.cs
@@ -0,0 +1,12 @@
+﻿using Day3;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int A = 10; int B = 20;
+        Cust c = new Cust(A, B);
+
+        c.CValue();
+    }
+}
